eb8c332d4b7a44d2162ea2c9a9e7c297ddbe0428
brooksolney <brooks.olney@gmail.com>
2023-01-20 15:45:34 -0500

    revert random excursions

--- tests/test_14_random_excursions.py
+++ tests/test_14_random_excursions.py
@@ -1,4 +1,3 @@
-from multiprocessing.pool import ThreadPool
 import numpy as np
 import scipy.special as ss
 
@@ -21,29 +20,19 @@ def random_excursion_test(binary):
     s = np.add.accumulate(bits, dtype=np.int16)
     s = np.pad(s, (1, 1), 'constant')
     zcrosses = np.argwhere(s == 0).reshape(-1)
-    inds2d = zcrosses.reshape(-1,2)
- 
-    test = [np.array(s[r[0]:r[1]]) for r in inds2d]
-    test2 = [t[(t >= -4) & (t <= 4)] + 4 for t in test]
 
-    with ThreadPool() as p:
-        counts = np.vstack([np.pad(r, [0, 9 - len(r)]) for r in p.imap(np.bincount, test2)])
-    
-    
-
-    states = np.zeros((9, 6), dtype=np.int64)
+    states = np.zeros((9, 6), dtype=np.int16)
     for i in range(len(zcrosses) - 1):
         subarr = s[zcrosses[i]:zcrosses[i+1]]
         subarr = subarr[(subarr >= -4) & (subarr <= 4)] + 4
 
-        counts = np.zeros(9, dtype=np.int64)
-        # for x in subarr:
-        #     counts[x] += 1
-        _counts = np.bincount(subarr)
-        counts[range(len(_counts))] += _counts
+        counts = np.zeros(9, dtype=np.uint8)
+        for x in subarr:
+            counts[x] += 1
         counts[counts > 5] = 5
         states[range(9), counts] += 1
 
+    # J = np.sum(states)
     J = len(zcrosses) - 1
 
     chisqs = np.zeros(9, dtype=np.float32)
